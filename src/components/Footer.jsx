70479d4fdd65dde0fdcba61e3859efb357de4884
TimotheGonin <gonin.timothe@gmail.com>
2023-10-23 12:20:50 +0200

    💄 add footer + primary styles

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Copyright } from "current-year-copyright-react";
+
+const Footer = () => {
+	return (
+		<footer className="p-3 bg-light">
+			<Copyright nameEntry="Timothé Gonin" />
+		</footer>
+	);
+};
+
+export default Footer;
