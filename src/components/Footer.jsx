7bb42f44b2e6bb1b0ea0c38ac36d0b1ddd65594a
TimotheGonin <gonin.timothe@gmail.com>
2023-10-23 13:24:18 +0200

    💄 sizing + font famiily

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { Copyright } from "current-year-copyright-react";
 
 const Footer = () => {
+	const customStyle = {
+		marginBottom: 0,
+		fontFamily: "Roboto",
+	};
 	return (
 		<footer className="p-3 bg-light">
-			<Copyright nameEntry="Timothé Gonin" />
+			<Copyright nameEntry="Timothé Gonin" customStyles={customStyle} />
 		</footer>
 	);
 };
