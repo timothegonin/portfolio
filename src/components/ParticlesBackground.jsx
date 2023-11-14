1d4d7a83182641e161caaa8d4f01abfbc24f4ce1
TimotheGonin <gonin.timothe@gmail.com>
2023-11-14 18:24:12 +0100

    💫 improve colors, animations, size

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -16,7 +16,7 @@ const ParticlesBackground = () => {
 				},
 				background: {
 					color: {
-						value: "#0d47a1",
+						value: "#fff",
 					},
 				},
 				fpsLimit: 120,
@@ -24,30 +24,39 @@ const ParticlesBackground = () => {
 					events: {
 						onClick: {
 							enable: true,
-							mode: "push",
+							mode: "repulse",
 						},
 						onHover: {
 							enable: true,
-							mode: "repulse",
+							mode: "attract",
 						},
 						resize: true,
 					},
 					modes: {
-						push: {
-							quantity: 4,
-						},
 						repulse: {
 							distance: 200,
 							duration: 0.4,
+							factor: 100,
+							speed: 1,
+							maxSpeed: 50,
+							easing: "ease-out-quad",
+						},
+						attract: {
+							distance: 200,
+							duration: 0.4,
+							easing: "ease-out-quad",
+							factor: 1,
+							maxSpeed: 50,
+							speed: 1,
 						},
 					},
 				},
 				particles: {
 					color: {
-						value: "#ffffff",
+						value: "#000",
 					},
 					links: {
-						color: "#ffffff",
+						color: "#000",
 						distance: 150,
 						enable: true,
 						opacity: 0.5,
@@ -60,7 +69,7 @@ const ParticlesBackground = () => {
 							default: "bounce",
 						},
 						random: false,
-						speed: 6,
+						speed: 3,
 						straight: false,
 					},
 					number: {
@@ -68,16 +77,19 @@ const ParticlesBackground = () => {
 							enable: true,
 							area: 800,
 						},
-						value: 80,
+						value: 100,
 					},
 					opacity: {
-						value: 0.5,
+						value: 0.7,
 					},
 					shape: {
+						close: true,
+						fill: true,
+						options: {},
 						type: "circle",
 					},
 					size: {
-						value: { min: 1, max: 5 },
+						value: { min: 1, max: 4 },
 					},
 				},
 				detectRetina: true,
