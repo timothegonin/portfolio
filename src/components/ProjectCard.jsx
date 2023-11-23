bbccab2b39b0e9816d885d9cf620ed80674c1eb5
TimotheGonin <gonin.timothe@gmail.com>
2023-11-23 09:28:46 +0100

    ✨ add offcanvas

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Carousel from "react-bootstrap/Carousel";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
+// import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 const ProjectCard = ({ title, image, link }) => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	console.log(image);
+
 	return (
 		<Card style={{ width: "18rem" }}>
 			{/* HEAD */}
 			<Card.Header className="p-1">
-				<Carousel fade data-bs-theme="dark">
+				{/* <Carousel fade data-bs-theme="dark">
 					{image.map((img, index) => (
 						<Carousel.Item key={index} className="px-5 pt-1 pb-4">
 							<Card.Img
@@ -19,7 +27,7 @@ const ProjectCard = ({ title, image, link }) => {
 							/>
 						</Carousel.Item>
 					))}
-				</Carousel>
+				</Carousel> */}
 				{/* <Card.Img
 					variant="top"
 					src={require(`../assets/preview/${image}`)}
@@ -36,11 +44,11 @@ const ProjectCard = ({ title, image, link }) => {
 			</Card.Body>
 			{/* FOOTER */}
 			<Card.Footer>
-				<ButtonGroup aria-label="Basic example">
-					<Button variant="outline-secondary" size="sm">
-						Infos
-					</Button>
-					<Button
+				{/* <ButtonGroup aria-label="Basic example"> */}
+				{/* <Button variant="outline-primary" size="sm">
+					Explorer
+				</Button> */}
+				{/* <Button
 						href={link}
 						target="_blank"
 						variant="outline-primary"
@@ -48,8 +56,39 @@ const ProjectCard = ({ title, image, link }) => {
 						role="button"
 					>
 						Accéder
-					</Button>
-				</ButtonGroup>
+					</Button> */}
+				{/* </ButtonGroup> */}
+				<Button variant="outline-primary" onClick={handleShow}>
+					Explorer
+				</Button>
+
+				<Offcanvas show={show} onHide={handleClose}>
+					<Offcanvas.Header closeButton>
+						<Offcanvas.Title>{title}</Offcanvas.Title>
+					</Offcanvas.Header>
+					<Offcanvas.Body>
+						<Carousel fade data-bs-theme="dark">
+							{image.map((img, index) => (
+								<Carousel.Item key={index} className="px-5 pt-1 pb-4">
+									<Card.Img
+										variant="top"
+										src={require(`../assets/preview/${img}`)}
+										style={{ objectFit: "scale-down", height: "18rem" }}
+									/>
+								</Carousel.Item>
+							))}
+						</Carousel>
+						<Button
+							href={link}
+							target="_blank"
+							variant="outline-primary"
+							size="sm"
+							role="button"
+						>
+							Accéder
+						</Button>
+					</Offcanvas.Body>
+				</Offcanvas>
 			</Card.Footer>
 		</Card>
 	);
