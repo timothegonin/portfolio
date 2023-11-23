1f3946c480f004d31a4af256033c2b80cbb23c9d
TimotheGonin <gonin.timothe@gmail.com>
2023-11-23 09:39:19 +0100

    🎨 remove useless/commented

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Carousel from "react-bootstrap/Carousel";
-// import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
 
@@ -11,23 +10,10 @@ const ProjectCard = ({ title, image, link }) => {
 	const handleClose = () => setShow(false);
 	const handleShow = () => setShow(true);
 
-	console.log(image);
-
 	return (
 		<Card style={{ width: "18rem" }}>
 			{/* HEAD */}
 			<Card.Header className="p-1">
-				{/* <Carousel fade data-bs-theme="dark">
-					{image.map((img, index) => (
-						<Carousel.Item key={index} className="px-5 pt-1 pb-4">
-							<Card.Img
-								variant="top"
-								src={require(`../assets/preview/${img}`)}
-								style={{ objectFit: "scale-down", height: "18rem" }}
-							/>
-						</Carousel.Item>
-					))}
-				</Carousel> */}
 				{/* <Card.Img
 					variant="top"
 					src={require(`../assets/preview/${image}`)}
@@ -44,20 +30,6 @@ const ProjectCard = ({ title, image, link }) => {
 			</Card.Body>
 			{/* FOOTER */}
 			<Card.Footer>
-				{/* <ButtonGroup aria-label="Basic example"> */}
-				{/* <Button variant="outline-primary" size="sm">
-					Explorer
-				</Button> */}
-				{/* <Button
-						href={link}
-						target="_blank"
-						variant="outline-primary"
-						size="sm"
-						role="button"
-					>
-						Accéder
-					</Button> */}
-				{/* </ButtonGroup> */}
 				<Button variant="outline-primary" onClick={handleShow}>
 					Explorer
 				</Button>
