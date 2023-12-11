9f133974607bf3e736d4d1d7ef0704cd1f4d7a1d
timotheG <gonin.timothe@gmail.com>
2023-12-11 15:59:29 +0100

    ⚗️ add switch button(TEST)

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,16 +1,31 @@
 import { useContext } from "react";
 import { UiContext } from "../utils/context/UiContext";
-import Button from "react-bootstrap/Button";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
+// import Button from "react-bootstrap/Button";
+// import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Form from "react-bootstrap/Form";
 
 const Controls = () => {
 	const { uiDirection, toogleUiDirection } = useContext(UiContext);
 	console.log(uiDirection);
 
+	// const handleUiDirection = (e) => {
+	// 	console.log(e.currentTarget.checked);
+	// };
+
 	return (
 		<>
 			<h4>Préférence de navigation :</h4>
-			<div>
+			<Form className="d-flex justify-content-center">
+				<Form.Check
+					type="switch"
+					id="custom-switch"
+					aria-label="Enable left-handed mode"
+					label="Check this switch"
+					// onClick={handleUiDirection}
+				/>
+			</Form>
+
+			{/* <div>
 				<ButtonGroup aria-label="Choose between left or right handed">
 					<Button
 						variant="outline-dark"
@@ -27,7 +42,7 @@ const Controls = () => {
 						Droitier
 					</Button>
 				</ButtonGroup>
-			</div>
+			</div> */}
 		</>
 	);
 };
