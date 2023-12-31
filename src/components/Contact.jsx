988fae103b1604777ad29a318b9c975be671e601
timotheG <gonin.timothe@gmail.com>
2023-12-31 12:32:00 +0100

    💄 header layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -51,7 +51,7 @@ const Contact = () => {
 		: "justify-content-start";
 
 	return (
-		<Container fluid className="px-3 py-2">
+		<Container fluid className="py-2">
 			<h3 className="visually-hidden">Contact me :</h3>
 			<Stack direction="horizontal" gap={3} className={contactIconsClass}>
 				<IconLink
