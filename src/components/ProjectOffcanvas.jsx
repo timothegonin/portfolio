ace2ef59ae0984d8319f884aa03a535fc9d68195
timotheG <gonin.timothe@gmail.com>
2024-01-12 08:23:58 +0100

    🚨 offcanvas close button

--- src/components/ProjectOffcanvas.jsx
+++ src/components/ProjectOffcanvas.jsx
@@ -38,7 +38,7 @@ const ProjectOffcanvas = ({
 	const offcanvasPlacement = !placement ? "end" : "start";
 	const offcanvasControlsPlacement = placement && "flex-row-reverse";
 	const buttonVariant = theme === "light" ? "outline-primary" : "outline-light";
-	const closeButtonVariant = theme === "light" ? "" : "white";
+	const closeButtonVariant = theme === "dark" && "dark";
 
 	return (
 		<React.Fragment>
@@ -54,12 +54,16 @@ const ProjectOffcanvas = ({
 				}  `}
 			>
 				<Offcanvas.Header
-					className={`"pb-0 ${offcanvasControlsPlacement} btn-close-white`}
+					className={`"pb-0 ${offcanvasControlsPlacement}`}
+					data-bs-theme={closeButtonVariant}
 				>
 					<Offcanvas.Title className={theme === "light" ? "" : "text-white"}>
 						{infos.title}
 					</Offcanvas.Title>
-					<CloseButton onClick={handleClose} variant={closeButtonVariant} />
+					<CloseButton
+						onClick={handleClose}
+						data-bs-theme={closeButtonVariant}
+					/>
 				</Offcanvas.Header>
 				<Offcanvas.Body>
 					<Stack gap={4}>
