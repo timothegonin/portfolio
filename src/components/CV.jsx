628faa4358485e14359523f7551296b510c62ab1
timotheG <gonin.timothe@gmail.com>
2024-01-15 17:29:56 +0100

    💄 dark mode

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,10 +1,37 @@
 import React, { useContext, useState } from "react";
 import { UiContext } from "../utils/context/UiContext";
 import { ThemeContext } from "../utils/context/ThemeContext";
+import styled from "styled-components";
+
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import pdfFile from "../assets/document/CV_Gonin_Timothé-2023.pdf";
 
+/* 
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ STYLE                                                                   │
+  └─────────────────────────────────────────────────────────────────────────┘
+ */
+const ModalWrapper = styled(Modal)`
+	.modal-content {
+		background-color: ${(props) =>
+			props.$theme === "light" ? "white" : "#212529"};
+		color: ${(props) => (props.$theme === "light" ? "#212529" : "white")};
+	}
+`;
+
+const ModalFooter = styled(Modal.Footer)`
+	border-top-width: 1px;
+	border-top-style: solid;
+	border-top-color: ${(props) =>
+		props.$theme === "light" ? "#C7C8C9" : "rgba(199,226,201,0.25)"};
+`;
+
+/* 
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ JSX                                                                     │
+  └─────────────────────────────────────────────────────────────────────────┘
+ */
 const CV = () => {
 	const { leftHandedMode } = useContext(UiContext);
 	const { theme } = useContext(ThemeContext);
@@ -22,23 +49,27 @@ const CV = () => {
 			<Button variant={buttonVariant} onClick={handleShow}>
 				Afficher mon CV
 			</Button>
-			<Modal
+			<ModalWrapper
 				show={show}
 				onHide={handleClose}
 				size="lg"
 				aria-labelledby="Modal containing a Curriculum Vitae in pdf format"
+				$theme={theme}
 			>
 				<Modal.Body style={{ height: "80vh" }}>
 					<iframe title="CV" src={pdfFile} height="100%" width="100%">
 						CV FILE
 					</iframe>
 				</Modal.Body>
-				<Modal.Footer className={`justify-content-${closeButtonPlacement}`}>
+				<ModalFooter
+					className={`justify-content-${closeButtonPlacement}`}
+					$theme={theme}
+				>
 					<Button variant={closeButtonVariant} onClick={handleClose}>
 						Fermer
 					</Button>
-				</Modal.Footer>
-			</Modal>
+				</ModalFooter>
+			</ModalWrapper>
 		</React.Fragment>
 	);
 };
