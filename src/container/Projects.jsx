1f9c042698cebef41ad793f26bcefdd2698da42a
timotheG <gonin.timothe@gmail.com>
2024-01-26 16:21:06 +0100

    📱 responsive layout

--- src/container/Projects.jsx
+++ src/container/Projects.jsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import ListGroup from "react-bootstrap/ListGroup";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import Stack from "react-bootstrap/Stack";
 import styled, { keyframes } from "styled-components";
 import Loader from "../components/Loader";
@@ -54,24 +56,29 @@ const Projects = ({ data }) => {
 	return projectsData.length === 0 ? (
 		<Loader title="Chargement" />
 	) : (
-		<section className="mb-4 p-2">
-			<Container fluid className="p-2 mb-4" style={{ maxWidth: "1000px" }}>
-				<Stack
-					direction="horizontal"
-					gap={2}
-					className="justify-content-center mb-4 "
-				>
-					<h3 className={`m-0 text-${titleVariant}`}>Projets</h3>
-				</Stack>
-
-				<ListGroup
-					data-bs-theme={theme === "dark" && "dark"}
-					className={`text-start `}
-				>
-					{listOfProjects}
-				</ListGroup>
-			</Container>
-		</section>
+		<Container
+			fluid
+			className="p-4 mb-4"
+			style={{ maxWidth: "850px" }}
+			as="section"
+		>
+			<Row>
+				<Col className="d-none d-sm-block bg-secondary text-white rounded-3 rounded-end-0 ">
+					PREVIEW
+				</Col>
+				<Col sm={6}>
+					<Stack direction="verical" gap={3}>
+						<h3 className={`m-0 text-${titleVariant} text-start`}>Projets</h3>
+						<ListGroup
+							data-bs-theme={theme === "dark" && "dark"}
+							className={`text-start `}
+						>
+							{listOfProjects}
+						</ListGroup>
+					</Stack>
+				</Col>
+			</Row>
+		</Container>
 	);
 };
 
