961e50b03da8e886eeeefecfeb22edc27518bc97
timotheG <gonin.timothe@gmail.com>
2024-01-30 16:26:04 +0100

    💄 navbar styles, darkmode+ background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
+import { useContext } from "react";
+import { ThemeContext } from "../utils/context/ThemeContext";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Theme from "./Theme";
 import LeftHandMode from "./LeftHandMode";
 
 const NavBar = () => {
+	const { theme } = useContext(ThemeContext);
 	return (
-		<Navbar className="bg-body-tertiary">
+		<Navbar data-bs-theme={theme === "dark" && "dark"}>
 			<Container className="py-2" style={{ maxWidth: "650px" }}>
 				<LeftHandMode />
 				<Theme />
