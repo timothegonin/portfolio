f3063d837e3c0dbba30878fe5bfbe71ddc72d7ac
timotheG <gonin.timothe@gmail.com>
2024-02-12 10:05:51 +0100

    ⚰️ removing useless/commented code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import Footer from "./components/Footer";
-// import LeftHandMode from "./components/LeftHandMode";
 import ParticlesBackground from "./components/ParticlesBackground";
-// import Theme from "./components/Theme";
 import NavBar from "./components/Navbar";
-// import Toogler from "./components/Toogler";
 import Header from "./container/Header";
 import Projects from "./container/Projects";
 import "./fonts.css";
 
 function App({ data }) {
 	return (
-		<div className="App" style={{ height: "100vh", minHeight: "100vh" }}>
+		<div className="App">
 			<ParticlesBackground />
 			<NavBar />
-			{/* <Toogler alignment="center" position="top">
-				<Theme />
-			</Toogler> */}
-			{/* <Toogler alignment="start" position="bottom">
-				<LeftHandMode />
-			</Toogler> */}
 			<Header />
 			<Projects data={data} />
 			<Footer />
