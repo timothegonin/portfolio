7c08ebdd92c78893aff3f7025631c0a1c4dd49eb
timotheG <gonin.timothe@gmail.com>
2024-02-15 16:26:38 +0100

    🚧:poop: add basic cursor code

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import ParticlesBackground from "./components/ParticlesBackground";
 import NavBar from "./components/Navbar";
 import Header from "./container/Header";
 import Projects from "./container/Projects";
+import "./components/Cursor/Cursor";
+import "./components/Cursor/Cursor.css";
 
 function App({ data }) {
 	return (
 		<div className="App">
+			<div className="cursor"></div>
 			<ParticlesBackground />
 			<NavBar />
 			<Header />
