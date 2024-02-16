48673e2368bece46a6f8db069b0c54fe10eb2754
timotheG <gonin.timothe@gmail.com>
2024-02-16 10:57:19 +0100

    🚧 🏗️ ✨new cursor code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import ParticlesBackground from "./components/ParticlesBackground";
 import NavBar from "./components/Navbar";
 import Header from "./container/Header";
 import Projects from "./container/Projects";
-import "./components/Cursor/Cursor";
-import "./components/Cursor/Cursor.css";
+import Cursor from "./components/Cursor/Cursor";
+// import "./components/Cursor/Cursor.css";
 
 function App({ data }) {
 	return (
 		<div className="App">
-			<div className="cursor"></div>
+			{/* <div className="cursor"></div> */}
+			<Cursor />
 			<ParticlesBackground />
 			<NavBar />
 			<Header />
