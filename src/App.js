15879aeec4e90572e38ed4e9e37decba633c6551
timotheG <gonin.timothe@gmail.com>
2025-09-05 13:50:50 +0200

    🚧 disabling custom cursor

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import ParticlesBackground from "./components/UI/ParticlesBackground";
 import NavBar from "./components/ToogleBar/Navbar";
 import Header from "./container/Header";
 import Projects from "./container/Projects";
-import Cursor from "./components/Cursor/Cursor";
+// import Cursor from "./components/Cursor/Cursor";
 
 function App({ data }) {
 	return (
 		<div className="App">
-			<Cursor />
+			{/* <Cursor /> */}
 			<ParticlesBackground />
 			<NavBar />
 			<Header />
